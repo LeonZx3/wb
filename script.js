16580ea48daab7d8789fc50d58cc993d590ccd11
LeonZx3 <altuveleonardo96@gmail.com>
2025-02-22 13:56:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('startScanner').addEventListener('click', function() {
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#scanner'),
+            constraints: {
+                width: 480,
+                height: 320,
+                facingMode: "environment" // Usar la cámara trasera
+            }
+        },
+        decoder: {
+            readers: ["ean_reader", "upc_reader", "code_39_reader", "code_128_reader"]
+        }
+    }, function(err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Inicialización exitosa. Escaneando...");
+        Quagga.start();
+    });
+
+    Quagga.onDetected(function(result) {
+        const code = result.codeResult.code;
+        const listItem = document.createElement('li');
+        listItem.textContent = code;
+        document.getElementById('scannedCodes').appendChild(listItem);
+    });
+});
